3c595fc0cdc4776aab269d05635ac7d11c3756a8
wjshan0808 <420028080@qq.com>
2022-10-02 20:21:02 +0800

    编写测试版本V1.0功能代码

--- Formula/FormulaTest.Designer.cs
+++ Formula/FormulaTest.Designer.cs
@@ -28,22 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtTestLog = new System.Windows.Forms.RichTextBox();
             this.lblVersion = new System.Windows.Forms.Label();
             this.cmbVersion = new System.Windows.Forms.ComboBox();
             this.btnTest = new System.Windows.Forms.Button();
+            this.txtTestLog = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // txtTestLog
-            // 
-            this.txtTestLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtTestLog.Location = new System.Drawing.Point(12, 37);
-            this.txtTestLog.Name = "txtTestLog";
-            this.txtTestLog.Size = new System.Drawing.Size(730, 390);
-            this.txtTestLog.TabIndex = 0;
-            this.txtTestLog.Text = "";
             // 
             // lblVersion
             // 
@@ -76,15 +65,27 @@
             this.btnTest.UseVisualStyleBackColor = true;
             this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
             // 
+            // txtTestLog
+            // 
+            this.txtTestLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTestLog.Location = new System.Drawing.Point(12, 37);
+            this.txtTestLog.Multiline = true;
+            this.txtTestLog.Name = "txtTestLog";
+            this.txtTestLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtTestLog.Size = new System.Drawing.Size(730, 390);
+            this.txtTestLog.TabIndex = 4;
+            // 
             // FormulaTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(754, 439);
+            this.Controls.Add(this.txtTestLog);
             this.Controls.Add(this.btnTest);
             this.Controls.Add(this.cmbVersion);
             this.Controls.Add(this.lblVersion);
-            this.Controls.Add(this.txtTestLog);
             this.Name = "FormulaTest";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FormulaTest_Load);
@@ -94,10 +95,9 @@
         }
 
         #endregion
-
-        private RichTextBox txtTestLog;
         private Label lblVersion;
         private ComboBox cmbVersion;
         private Button btnTest;
+        private TextBox txtTestLog;
     }
 }
--- Formula/FormulaTest.cs
+++ Formula/FormulaTest.cs
@@ -23,6 +23,128 @@ namespace Formula
 
         private void btnTest_Click(object sender, EventArgs e)
         {
+            FormulaVersion enVersion = (FormulaVersion)this.cmbVersion.SelectedItem;
+
+            switch (enVersion)
+            {
+                case FormulaVersion.FV10:
+                    TEST_V10();
+                    break;
+                case FormulaVersion.None:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        public void TEST_V10()
+        {
+            string strFormula = string.Empty;
+            string strResult = string.Empty;
+            double dResult = 0.0;
+
+            //
+            TEST_Version(FormulaVersion.FV10);
+
+            //空
+            strFormula = "  ";
+            dResult = 0;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+
+            //单字符
+            strFormula = " 0 ";
+            dResult = 0;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+            strFormula = " - 0 ";
+            dResult = -0;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+
+            //单运算
+            strFormula = " 0- 1 ";
+            dResult = 0 - 1;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+            strFormula = " 2 /3.0 ";
+            dResult = 2 / 3.0;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+
+            //双运算
+            strFormula = " 0- 1+ 3 ";
+            dResult = 0 - 1 + 3;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+            strFormula = " 2 /3.0 *4";
+            dResult = 2 / 3.0 * 4;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+
+            //混合双运算
+            strFormula = " -0* 1+ 3 ";
+            dResult = -0 * 1 + 3;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+            strFormula = " 2 /3.0 --4";
+            dResult = 2 / 3.0 - -4;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+
+            //三运算
+            strFormula = "1 -0- 1+ 3 ";
+            dResult = 1 - 0 - 1 + 3;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+            strFormula = "1* 2 /3 /+4";
+            dResult = 1 * 2 / 3 / +4;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+
+            //混合三运算
+            strFormula = "1 -0/ 1+ -3 ";
+            dResult = 1 - 0 / 1 + -3;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+            strFormula = "-1* 2 -3 /-4";
+            dResult = -1 * 2 - 3 / -4;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+
+            //四运算
+            strFormula = "1 -0- 1+ 3* 4/5.6 ";
+            dResult = 1 - 0 - 1 + 3 * 4 / 5.6;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+            strFormula = "1 /2 *3  +4-+5";
+            dResult = 1 / 2 * 3 + 4 - +5;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+
+            //混合四运算
+            strFormula = "-1/ -3+ 1 -0 * 4 ";
+            dResult = -1 / -3 + 1 - 0 * 4;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+            strFormula = "+0--1* 2 /-3.5 +-4";
+            dResult = +0 - -1 * 2 / -3.5 + -4;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+
+            //混合运算
+            strFormula = "0-1/ -3+ 1 -0 * 4 /9*8.7/7.7-6.5";
+            dResult = 0 - 1 / -3 + 1 - 0 * 4 / 9 * 8.7 / 7.7 - 6.5;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+            strFormula = "1*0.0-1- -3+ 1 -0 * 4 /9*8.7/7.7-6.5+0--1* 2 /-3.5 +-4/4.0";
+            dResult = 1 * 0.0 - 1 - -3 + 1 - 0 * 4 / 9 * 8.7 / 7.7 - 6.5 + 0 - -1 * 2 / -3.5 + -4 / 4.0;
+            TEST_Item(strFormula, dResult, Formula.Calculate(strFormula, FormulaVersion.FV10));
+
+
+        }
+
+
+        private void TEST_Item(string strFormula, double dResult, string strResult)
+        {
+            //
+            txtTestLog.Text += Environment.NewLine;
+            txtTestLog.Text += strFormula;
+            txtTestLog.Text += Environment.NewLine;
+            txtTestLog.Text += string.Format("[{0}] <=> [{1}]", dResult, strResult);
+            txtTestLog.Text += Environment.NewLine;
+
+        }
+        private void TEST_Version(FormulaVersion enVersion)
+        {
+            //
+            txtTestLog.Text += Environment.NewLine;
+            txtTestLog.Text += string.Format("---------{0}---------", enVersion.ToString());
+            txtTestLog.Text += Environment.NewLine;
+            txtTestLog.Text += Environment.NewLine;
 
         }
     }
